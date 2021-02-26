f57fb4aa277a2856820a1944e9b82470e815bd89
Ian <iancoder@1097@gmail.com>
2021-02-26 16:39:28 -0600

    ignore

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.jpg$/,
+        test: /\.(png|jpg)$/,
         use: ['file-loader']
       }
     ]
