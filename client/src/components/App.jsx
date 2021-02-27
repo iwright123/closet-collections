db6aa36e9a2c665b05488ca4a1d9456cebb23bf6
Ian <iancoder@1097@gmail.com>
2021-02-27 11:47:21 -0600

    working on oauth

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import GoogleButton from 'react-google-button';
 
 const App = () => {
 
   return (
     <div>
       <h1>MADE IT!</h1>
+      <a className='loginButton' href="/auth/google"> <GoogleButton /></a>
     </div>
+
   );
 
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,59 @@
 const path = require('path');
 const express = require('express');
+const { GoogleStrategy } = require('./passport.js');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const app = express();
 
 const port = 3000;
-
 const dist = path.resolve(__dirname, '..', 'client', 'dist');
-const app = express();
+
+
 app.use(express.static(dist));
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.clientSecret,
+    saveUninitialized: false,
+    resave: true,
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // setting cookie key to thesis and saving the username
+    res.cookie('thesis', req.user.displayName);
+    res.redirect('/');
+  });
+
+app.get('/isloggedin', (req, res) => {
+  // check to see if the cookie key is headstrong
+  if (req.cookies.thesis) {
+    res.json(true);
+  } else {
+    res.json(false);
+  }
+});
+
+app.delete('/logout', (req, res) => {
+  // delete the cookie key headstrong when logging out
+  res.clearCookie('thesis');
+  res.json(false);
+});
 
 
 app.listen(port, () => {
