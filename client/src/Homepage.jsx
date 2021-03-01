6252a39bcd1df893217643bd630eb5fa70604de6
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-03-01 16:04:18 -0600

    Working on routing

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -11,12 +11,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const Homepage = () => {
   return (
     <div className='App'>
+      <h1>David Can You See me</h1>
+
+
       <Router>
-
-        <Sidebar />
-        <Switch>
-
-        </Switch>
+      <Route path='/' exact component={Home} />
+        <Route path='/calendar' component={Calendar} />
+        <Route path='/outfits' component={Outfits} />
+        <Route path='/closet' component={Closet} />
       </Router>
     </div>
   );
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GoogleButton from 'react-google-button';
 import Homepage from '../Homepage.jsx';
+import axios from 'axios'
 
 const App = () => {
+const [login, setLogin] = useState(false);
+
+function isLoggedIn(){
+  setLogin(login => login = !login)
+}
+
 
   return (
     <div>
       <h1>MADE IT!</h1>
-      <a className='loginButton' href="/auth/google"> <GoogleButton /></a>
+      <a onClick={isLoggedIn} className='loginButton' href="/auth/google"> <GoogleButton /></a>
       <Homepage />
     </div>
 
