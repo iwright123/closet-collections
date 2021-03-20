785f190229dba5cb73e34bba82042bb2461999d0
Ian <iancoder@1097@gmail.com>
2021-03-20 12:14:15 -0500

    added weather back

--- server/helpers/weather.ts
+++ server/helpers/weather.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 export const getWeatherByGeoLocation = (latitude: number, longitude: number): any => {
 
   const url = `http://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${process.env.WEATHERBIT_TOKEN}`;
